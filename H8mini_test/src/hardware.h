a0e04d4e014fcbcca6ca8cd91766a43a0c8be1a3
silver13 <a@a.com>
2017-01-11 08:06:18 +1030

    Revert "added new pwm driver by bikemike"
    This reverts commit 02a1ddba507d803fb2538e730cee11b2270b749a.

--- H8mini_test/src/hardware.h
+++ H8mini_test/src/hardware.h
@@ -49,27 +49,3 @@
 // enable serial out on back-left LED
 //#define SERIAL
 
-
-// pwm pin initialization
-// enable the pwm pins to be used here ( multiple pins ok)
-//#define PWM_PA0
-//#define PWM_PA1
-//#define PWM_PA2
-//#define PWM_PA3
-//#define PWM_PA4
-//#define PWM_PA5
-//#define PWM_PA6
-//#define PWM_PA7
-#define PWM_PA8
-#define PWM_PA9
-#define PWM_PA10
-//#define PWM_PA11
-//#define PWM_PB0
-#define PWM_PB1
-
-// Assingment of pin to motor
-// Assign one pin to one motor
-#define PA9_MOTOR_ID MOTOR_BL // bl
-#define PA8_MOTOR_ID MOTOR_FL
-#define PA10_MOTOR_ID MOTOR_FR
-#define PB1_MOTOR_ID MOTOR_BR // br
